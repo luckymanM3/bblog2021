9695a99511c6a4d5b2ac189456e11f9fce767748
SuperdevTim <timrober1992@gmail.com>
2021-04-16 00:58:17 +0700

    feat: added backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9c4bbbb5411e854551f791ff105aab76'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ad67ab4-11d9-4332-ba5d-78686589d3c7'
+};
